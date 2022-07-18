3d0afa138dd6160b49647904337e3abb3ad9c2ee
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-18 16:49:17 +0900

    feat: toDo list 타입 설정 및 상태 저장 추가

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,7 +1,7 @@
 import { useForm } from "react-hook-form";
 import { atom, useRecoilState } from "recoil";
 
-const toDoState = atom({
+const toDoState = atom<IToDo[]>({
   key: "toDoList",
   default: [],
 });
@@ -10,12 +10,22 @@ interface IForm {
   toDo: string;
 }
 
+interface IToDo {
+  text: string;
+  id: number;
+  category: "TO_DO" | "DOING" | "DONE";
+}
+
 function ToDoList() {
   const [toDos, setToDos] = useRecoilState(toDoState);
   const { register, handleSubmit, setValue } = useForm<IForm>();
 
-  const handleValid = (data: IForm) => {
-    console.log("add to do", data.toDo);
+  const handleValid = ({ toDo }: IForm) => {
+    console.log("add to do", toDo);
+    setToDos((oldToDos) => [
+      { text: toDo, id: Date.now(), category: "TO_DO" },
+      ...oldToDos,
+    ]);
     setValue("toDo", "");
   };
 
