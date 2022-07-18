e07db2d27626297e95484b8e6e320e34144892d0
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-18 20:37:45 +0900

    feat: list 버튼 클릭에 따른 상태 변경 구현

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { IToDo } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     const {
       currentTarget: { name },
     } = event;
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
   };
   return (
     <li>
