c68e63f8d7cd89c95f326854bd5f74d03a64950b
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-18 18:51:23 +0900

    feat: list 카테고리 버튼 마크업

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,6 +1,31 @@
+import React from "react";
 import { IToDo } from "../atoms";
 
 function ToDo({ text, category, id }: IToDo) {
-  return <li>{text}</li>;
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+  };
+  return (
+    <li>
+      <span>{text}</span>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
+    </li>
+  );
 }
 export default ToDo;
