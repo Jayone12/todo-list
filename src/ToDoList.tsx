6c39bc1e58870ae1c8dcfc75d239f6cc36102d10
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-18 13:40:45 +0900

    feat: Create ToDoList component and markup

---
+++ src/ToDoList.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function ToDoList() {
+  return (
+    <div>
+      <form>
+        <input placeholder="Write a to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default ToDoList;
