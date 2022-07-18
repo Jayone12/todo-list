7a3b68566c8debd795763d476de3a494070c0a84
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-18 13:07:53 +0900

    Todo List 프로젝트 초기 셋팅

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { DefaultTheme } from "styled-components";
+
+export const darkTheme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#fff",
+  accentColor: "#9c88ff",
+  cardBgColor: "#262c35",
+};
