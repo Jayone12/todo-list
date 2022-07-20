3b10d2c6f9bf69fb3be83953a38d0b914047f2e6
BigJay <53748573+Jayone12@users.noreply.github.com>
2022-07-20 12:00:07 +0900

    code test를 위해 초기화

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,38 +1,4 @@
-import React from "react";
-import { useSetRecoilState } from "recoil";
-import { Categories, IToDo, toDoState } from "../atoms";
-
-function ToDo({ text, category, id }: IToDo) {
-  const setToDos = useSetRecoilState(toDoState);
-  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    const {
-      currentTarget: { name },
-    } = event;
-    setToDos((oldToDos) => {
-      return oldToDos.map((toDo) =>
-        toDo.id === id ? { text, id, category: name as Categories } : toDo
-      );
-    });
-  };
-  return (
-    <li>
-      <span>{text}</span>
-      {category !== Categories.DOING && (
-        <button name={Categories.DOING} onClick={onClick}>
-          Doing
-        </button>
-      )}
-      {category !== Categories.TO_DO && (
-        <button name={Categories.TO_DO} onClick={onClick}>
-          To Do
-        </button>
-      )}
-      {category !== Categories.DONE && (
-        <button name={Categories.DONE} onClick={onClick}>
-          Done
-        </button>
-      )}
-    </li>
-  );
+function ToDo() {
+  return {};
 }
 export default ToDo;
